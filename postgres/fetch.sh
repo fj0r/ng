e96a5b11cbad911aaa36a241e90f2e635e535b8f
nash <nash@iffy.me>
2021-08-17 15:29:28 +0800

    update

---
+++ postgres/fetch.sh
@@ -0,0 +1,3 @@
+xh -o initdb-postgis.sh.ori https://raw.githubusercontent.com/postgis/docker-postgis/master/13-master/initdb-postgis.sh
+xh -o update-postgis.sh.ori https://raw.githubusercontent.com/postgis/docker-postgis/master/13-master/update-postgis.sh
+xh -o docker-entrypoint.sh.ori https://raw.githubusercontent.com/docker-library/postgres/master/13/buster/docker-entrypoint.sh
