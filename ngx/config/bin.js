420053ddea497735fb9a2c75680815951cfbf3ac
nash <nash@iffy.me>
2022-02-09 17:32:46 +0800

    upgrade ngx

--- ngx/config/bin.js
+++ ngx/config/bin.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises
 
-function bin(r) {
-    var f = METHODS[r.variables[1]]
+function run(r) {
+    var f = METHODS[r.variables.op]
     f ? f(r) : r.return(200, JSON.stringify({avaiables: Object.keys(METHODS)}))
 }
 
 const METHODS = {
     headers : r => r.return(200, JSON.stringify(r.headersIn)),
-    ip      : r => r.return(200, r.headersIn['X-Real-IP']),
+    ip      : r => r.return(200, r.headersIn['X-Real-IP'] || r.remoteAddress),
     rdr     : r => r.return(301, r.args.url),
     ua      : r => r.return(200, r.headersIn['User-Agent']),
     body    : r => r.return(200, r.requestBuffer),
@@ -18,38 +18,4 @@ const METHODS = {
     v       : r => r.return(200, JSON.stringify(r.variables[r.args.v])),
 }
 
-
-function check_scheme(url) {
-  if (url.startsWith('http')) {
-    return url
-  } else {
-    return `http://${url}`
-  }
-}
-
-function get_host(url) {
-  if (url.startsWith('http')) {
-    return url.split(':')[1].slice(2)
-  } else {
-    return url.split(':')[0]
-  }
-}
-
-function _env(n) {
-  return process.env[`UPSTREAM_${n.toUpperCase()}`]
-}
-
-//////////////////////// {{{
-
-function u_test(r) {
-  return check_scheme(_env('test'))
-}
-
-function h_test(r) {
-  return get_host(_env('test'))
-}
-
-export default { bin
-               , u_test
-               , h_test
-               }
+export default { run }
