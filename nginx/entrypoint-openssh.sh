d42e46f7f14d7e56fd597b051a9fab6da4fde693
nash <nash@iffy.me>
2022-02-18 23:03:44 +0800

    switch to dropbear

--- nginx/entrypoint-openssh.sh
+++ nginx/entrypoint-openssh.sh
@@ -7,7 +7,21 @@ echo "[$(date -Is)] starting sshd"
 ################################################################################
 if [[ "$DEBUG" == 'true' ]]; then
     set -x
+    sed -i /etc/ssh/sshd_config \
+        -e 's!.*\(LogLevel\).*!\1 DEBUG!'
 fi
+
+DAEMON=sshd
+
+print_fingerprints() {
+    local BASE_DIR=${1-'/etc/ssh'}
+    for item in rsa ecdsa ed25519; do
+        echo ">>> Fingerprints for ${item} host key"
+        ssh-keygen -E md5 -lf ${BASE_DIR}/ssh_host_${item}_key
+        ssh-keygen -E sha256 -lf ${BASE_DIR}/ssh_host_${item}_key
+        ssh-keygen -E sha512 -lf ${BASE_DIR}/ssh_host_${item}_key
+    done
+}
 
 # Add users if $1=user:uid:gid set
 set_user () {
@@ -21,6 +35,20 @@ set_user () {
 }
 
 init_ssh () {
+    if [[ "${SSH_OVERRIDE_HOST_KEYS}" == "true" ]]; then
+        rm -rf /etc/ssh/ssh_host_*
+    fi
+    # Generate Host keys, if required
+    if ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+        echo ">> Host keys exist in default location"
+        # Don't do anything
+        print_fingerprints
+    else
+        echo ">> Generating new host keys"
+        ssh-keygen -A
+        print_fingerprints /etc/ssh
+    fi
+
     if [ -n "$user" ]; then
         for u in $(echo $user | tr "," "\n"); do
             set_user ${u} 'SSH User'
@@ -44,6 +72,18 @@ init_ssh () {
         chown root:root ~/.ssh/authorized_keys
         chmod 600 ~/.ssh/authorized_keys
     fi
+
+    # Lock root account, if Disabled
+    if [[ "${SSH_DISABLE_ROOT}" == "true" ]]; then
+        echo "WARNING: root account is now locked. Unset SSH_DISABLE_ROOT to unlock the account."
+    else
+        usermod -p '' root
+    fi
+
+    # Update MOTD
+    if [ -v MOTD ]; then
+        echo -e "$MOTD" > /etc/motd
+    fi
 }
 
 
@@ -66,7 +106,7 @@ env | grep -E '_|HOME|ROOT|PATH|VERSION|LANG|TIME|MODULE|BUFFERED' \
 
 trap stop SIGINT SIGTERM #ERR EXIT
 init_ssh
-/usr/bin/dropbear -REFms -p 22 &
+/usr/sbin/sshd -D -e 2>&1 &
 echo -n "$! " >> /var/run/services
 
 ################################################################################
--- ngx/start.sh
+++ ngx/start.sh
@@ -9,15 +9,35 @@ trap stop SIGINT SIGTERM SIGQUIT
 nginx &
 ngx=$!
 
-ssh-keygen -A
 
-mkdir -p /etc/ssh/authorized_keys
-for i in "${!ed25519_@}"; do
-    _AU=${i:8}
-    eval "echo \"ssh-ed25519 \$$i\" >> /etc/ssh/authorized_keys/${_AU}"
-done
+init_ssh () {
+    if [ -n "$user" ]; then
+        for u in $(echo $user | tr "," "\n"); do
+            set_user ${u} 'SSH User'
+        done
+    fi
 
-/usr/sbin/sshd -D -e &
+    for i in "${!ed25519_@}"; do
+        _AU=${i:8}
+        _HOME_DIR=$(getent passwd ${_AU} | cut -d: -f6)
+        mkdir -p ${_HOME_DIR}/.ssh
+        eval "echo \"ssh-ed25519 \$$i\" >> ${_HOME_DIR}/.ssh/authorized_keys"
+        chown ${_AU} -R ${_HOME_DIR}/.ssh
+        chmod go-rwx -R ${_HOME_DIR}/.ssh
+    done
+
+    # Fix permissions, if writable
+    if [ -w ~/.ssh ]; then
+        chown root:root ~/.ssh && chmod 700 ~/.ssh/
+    fi
+    if [ -w ~/.ssh/authorized_keys ]; then
+        chown root:root ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+    fi
+}
+
+init_ssh
+/usr/bin/dropbear -REFms -p 22 &
 sshd=$!
 
 wait -n $ngx $sshd
